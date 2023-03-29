628e43a8493893eac6c1fb719cc05f42ab3a76fb
itwill <itwill@DESKTOP-GLGAE9R>
2023-03-29 12:32:14 +0900

    1days

---
+++ java_1/src/basic/FirstEclipseApp.java
@@ -0,0 +1,29 @@
+//2023-03-28 
+/* java project 01 */
+
+package basic; //클래스가 작성된 패키지를 표현하기 위한 문장
+
+//주석문 : 프로그램에 설명을 제공하기 위한 문장
+//=>프로그램 실행과 아무런 관계없는 문장
+//=>행주석 또는 범위주석
+/*
+범위주석 : 범위의 문장을 설명문으로 설정하여 사용
+*/
+
+//이클립스 단축키
+//[ctrl]+[space] => 단어(키워드 또는 식별자)와 문장(템플릿 코드)을 자동 완성하는 기능 제공
+//[ctrl]+[alt]+[↑] 또는 [ctrl]+[alt]+[↓] : 행을 복사하여 붙여넣기 하는 기능 제공
+//[alt]+[↑] 또는 [alt]+[↓] : 행을 이동하는 기능 제공
+//[ctrl]+[s] : 저장
+//[ctrl]+[/] : 주석 토글 //사용 
+//[ctrl]+[shitf]+[/] 또는[ctrl]+[shitf]+[\]  : 블록 주석 추가/삭제
+//[ctrl]+[f11] : Run(실행)
+//[f11] : debug 
+
+public class FirstEclipseApp {
+	public static void main(String[] args) {
+		System.out.print("안녕하세요."); //mysys 작성 후 ctrl+space 
+		System.out.println("이클립스를 이용한 첫번째 프로그램입니다."); //sysout 작성 후 ctrl+space
+		System.out.println("재미있게 프로그램을 만들어 봐요.");
+	}
+} 
