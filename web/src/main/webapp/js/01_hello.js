0447e4c1c581e7e3e3fbd415d16f4e36cfdbc794
user <user@BOOK-LCCUUB9T5H>
2023-06-02 17:12:41 +0900

    2023-06-02

---
+++ web/src/main/webapp/js/01_hello.js
@@ -0,0 +1,8 @@
+/* 
+	자바스크립트 파일에는 명령보다는 함수나 클래스를 만듦. 
+*/
+alert("JavaScript Source File에 작성된 자바스크립트 명령"); //01_hello.html--58줄
+
+function eventHandle(){
+	alert("이벤트 처리 함수의 명령 실행"); //01_hello.html--63줄
+}
