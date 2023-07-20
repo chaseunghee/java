d21edf0e1c7c570206e7160d19e97132d692e73b
user <user@BOOK-LCCUUB9T5H>
2023-07-20 18:11:33 +0900

    2023-07-20

--- mybatis/src/main/java/xyz/itwill/dao/MyCommentDAO.java
+++ mybatis/src/main/java/xyz/itwill/dao/MyCommentDAO.java
@@ -7,6 +7,8 @@ import org.apache.ibatis.session.SqlSession;
 import xyz.itwill.dto.MyComment1;
 import xyz.itwill.dto.MyComment2;
 import xyz.itwill.dto.MyComment3;
+import xyz.itwill.dto.MyCommentReply;
+import xyz.itwill.dto.MyCommentReplyUser;
 import xyz.itwill.dto.MyCommentUser1;
 import xyz.itwill.dto.MyCommentUser2;
 import xyz.itwill.dto.MyReply;
@@ -103,6 +105,24 @@ public class MyCommentDAO extends AbstractSession {
 		SqlSession sqlSession=getSqlSessionFactory().openSession(true);
 		try {
 			return sqlSession.getMapper(MyCommentMapper.class).selectCommentNoReplyList(commentNo);
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	public MyCommentReply selectCommentReply(int commentNo) {
+		SqlSession sqlSession=getSqlSessionFactory().openSession(true);
+		try {
+			return sqlSession.getMapper(MyCommentMapper.class).selectCommentReply(commentNo);
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	public MyCommentReplyUser selectCommentReplyUser(int commentNo) {
+		SqlSession sqlSession=getSqlSessionFactory().openSession(true);
+		try {
+			return sqlSession.getMapper(MyCommentMapper.class).selectCommentReplyUser(commentNo);
 		} finally {
 			sqlSession.close();
 		}
