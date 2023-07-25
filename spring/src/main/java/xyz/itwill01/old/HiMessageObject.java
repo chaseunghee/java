77b843d5cc74293f645075f938441f2b7fbabb2b
user <user@BOOK-LCCUUB9T5H>
2023-07-25 17:28:12 +0900

    2023-07-25

---
+++ spring/src/main/java/xyz/itwill01/old/HiMessageObject.java
@@ -0,0 +1,7 @@
+package xyz.itwill01.old;
+
+public class HiMessageObject {
+	public String getHelloMessage() {
+		return "Hi!!!";
+	}
+}
---
+++ spring/src/main/java/xyz/itwill02/factory/HiMessageObejct.java
@@ -0,0 +1,9 @@
+package xyz.itwill02.factory;
+
+public class HiMessageObejct implements MessageObject{
+	@Override
+	public String getMessage() {
+		return "Hi";
+	}
+
+}
